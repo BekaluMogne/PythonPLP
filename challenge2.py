f1dee57d2dd432e3043dfc230c98e66ae29fd49e
BekaluMogne <48547712+BekaluMogne@users.noreply.github.com>
2025-08-14 02:06:33 -0700

    Add files via upload

---
+++ challenge2.py
@@ -0,0 +1,14 @@
+price=input('Enter the original price')
+discount_percent=input('Enter the discount')
+price=float(price)
+discount_percent=float(discount_percent)
+discountpercent=float(discount_percent*0.01)
+
+def calculate_discount(price,discount_percent):
+    if(float(discount_percent))>=20:
+      discountPrice=float(price-(price*discountpercent))
+      return float(discountPrice)
+    else:
+        return price
+
+print(float(calculate_discount(price,discount_percent)) )
